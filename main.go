3254910e51de12f6ef27b9687ddb97f3426429e2
hotkimho <rlaghekd777@naver.com>
2022-06-22 02:26:08 +0900

    Add. Connect mysql

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
 )
+
+func createTable(db *sql.DB) error {
+	createTableQuery := `
+	CREATE TABLE IF NOT EXISTS
+	
+	
+`
+}
 
 func main() {
 	config := godotenv.Load(".env")
 	if config != nil {
 		log.Fatal("Error loading dotenv")
 	}
-	fmt.Println(os.Getenv("TEST"))
+
+	db, err := sql.Open("mysql", os.Getenv("DBURL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("ok", db.Ping())
 }
