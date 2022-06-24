c9f33b179ce349e398f855a97f1f26f126e12f7c
hokim <rlaghekd777@naver.com>
2022-06-24 22:41:13 +0900

    Add. SignupRequest model, Session model

--- model/user.go
+++ model/user.go
@@ -1,1 +1,20 @@
 package model
+
+type SignupRequestUser struct {
+	Username string
+	Password string
+}
+
+// Session
+// 생성한 Query
+// CREATE TABLE IF NOT EXISTS user_session (
+// session_id TEXT NOT NULL,
+// user_id binary(16) NOT NULL,
+// FOREIGN KEY(user_id)
+// REFERENCES user(uuid)
+// );
+///*
+type Session struct {
+	SessionId string
+	UserId    string
+}
